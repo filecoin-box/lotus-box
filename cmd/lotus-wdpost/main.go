ad0a672d32bdf95c86643ec72d530baeb706ff67
luluup777 <luluup777@163.com>
2021-11-22 11:30:39 +0800

    add lotus-wdpost tools

---
+++ cmd/lotus-wdpost/main.go
@@ -0,0 +1,390 @@
+package main
+
+import (
+	"context"
+	"crypto/rand"
+	addr "github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-bitfield"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/blockstore"
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
+	"github.com/filecoin-project/lotus/chain/types"
+	lcli "github.com/filecoin-project/lotus/cli"
+	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper"
+	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper/basicfs"
+	"github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
+	cbor "github.com/ipfs/go-ipld-cbor"
+	logging "github.com/ipfs/go-log/v2"
+	"github.com/urfave/cli/v2"
+	"golang.org/x/xerrors"
+	"lotus-box/util"
+	"strconv"
+	"strings"
+)
+
+var log = logging.Logger("emulator")
+
+func main() {
+	_ = logging.SetLogLevel("*", "INFO")
+
+	app := &cli.App{
+		Name:                 "lotus-wdpost",
+		Usage:                "lotus wdpost emulator",
+		Version:              "v0.1",
+		EnableBashCompletion: true,
+		Commands: []*cli.Command{
+			sectorEmulator,
+			partitionEmulator,
+			deadlineEmulator,
+		},
+	}
+
+	app.Setup()
+	lcli.RunApp(app)
+}
+
+var sectorEmulator = &cli.Command{
+	Name:  "s-emulator",
+	Usage: "sector WindowPost simulator",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:  "sids",
+			Usage: "simulate sector ids, if there are more than one, separate commas. ps: 1,2",
+			Value: "",
+		}, &cli.StringFlag{
+			Name:  "sdir",
+			Usage: "The directory where the redo sector is stored",
+			Value: "",
+		},
+		&cli.StringFlag{
+			Name:  "actor",
+			Usage: "miner actor id",
+		},
+	},
+	Action: func(cctx *cli.Context) error {
+		nodeApi, closer, err := lcli.GetFullNodeAPIV1(cctx)
+		if err != nil {
+			return err
+		}
+		defer closer()
+
+		sidsStr := cctx.String("sids")
+		sids := strings.Split(sidsStr, ",")
+
+		sbit := bitfield.New()
+		for _, id := range sids {
+			sid, err := strconv.Atoi(id)
+			if err != nil {
+				log.Warnw("sector id parsing failed", "id", id)
+				continue
+			}
+			sbit.Set(uint64(sid))
+		}
+
+		sdir, err := getSdir(cctx)
+		if err != nil {
+			return err
+		}
+
+		maddr, err := util.GetActorAddress(cctx)
+		if err != nil {
+			return err
+		}
+
+		amid, err := addr.IDFromAddress(maddr)
+		if err != nil {
+			return err
+		}
+
+		sInfo, err := getSectorInfo(nodeApi, maddr, sbit)
+		if err != nil {
+			return err
+		}
+
+		sb, err := ffiwrapper.New(&basicfs.Provider{
+			Root: sdir,
+		})
+		if err != nil {
+			return err
+		}
+
+		err = emulator(sb, abi.ActorID(amid), sInfo)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	},
+}
+
+var partitionEmulator = &cli.Command{
+	Name:  "p-emulator",
+	Usage: "partition WindowPost simulator",
+	Flags: []cli.Flag{
+		&cli.IntFlag{
+			Name:  "deadline",
+			Usage: "deadline id",
+			Value: 0,
+		}, &cli.IntFlag{
+			Name:  "partition",
+			Usage: "partition id",
+			Value: 0,
+		}, &cli.StringFlag{
+			Name:  "sdir",
+			Usage: "The directory where the redo sector is stored",
+			Value: "",
+		},
+		&cli.StringFlag{
+			Name:  "actor",
+			Usage: "miner actor id",
+		},
+	},
+	Action: func(cctx *cli.Context) error {
+		deadlineID := cctx.Int("deadline")
+		partitionID := cctx.Int("partition")
+		if deadlineID < 0 || deadlineID > 47 {
+			return xerrors.New("--deadline must be between 0 and 47")
+		}
+
+		nodeApi, closer, err := lcli.GetFullNodeAPIV1(cctx)
+		if err != nil {
+			return err
+		}
+		defer closer()
+
+		maddr, err := util.GetActorAddress(cctx)
+		if err != nil {
+			return err
+		}
+
+		amid, err := addr.IDFromAddress(maddr)
+		if err != nil {
+			return err
+		}
+
+		head, err := nodeApi.ChainHead(context.Background())
+		if err != nil {
+			return err
+		}
+
+		mact, err := nodeApi.StateGetActor(context.Background(), maddr, head.Key())
+		if err != nil {
+			return err
+		}
+
+		tbs := blockstore.NewTieredBstore(blockstore.NewAPIBlockstore(nodeApi), blockstore.NewMemory())
+		mas, err := miner.Load(adt.WrapStore(context.Background(), cbor.NewCborStore(tbs)), mact)
+		if err != nil {
+			return err
+		}
+
+		dl, err := mas.LoadDeadline(uint64(deadlineID))
+		if err != nil {
+			return err
+		}
+
+		part, err := dl.LoadPartition(uint64(partitionID))
+		if err != nil {
+			return err
+		}
+
+		liveSector, err := part.LiveSectors()
+		if err != nil {
+			return err
+		}
+
+		sInfo, err := getSectorInfo(nodeApi, maddr, liveSector)
+		if err != nil {
+			return err
+		}
+
+		sdir, err := getSdir(cctx)
+		if err != nil {
+			return err
+		}
+
+		sb, err := ffiwrapper.New(&basicfs.Provider{
+			Root: sdir,
+		})
+		if err != nil {
+			return err
+		}
+
+		err = emulator(sb, abi.ActorID(amid), sInfo)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	},
+}
+var deadlineEmulator = &cli.Command{
+	Name:  "d-emulator",
+	Usage: "deadline WindowPost simulator",
+	Flags: []cli.Flag{
+		&cli.IntFlag{
+			Name:  "deadline",
+			Usage: "deadline id",
+			Value: 0,
+		}, &cli.StringFlag{
+			Name:  "sdir",
+			Usage: "The directory where the redo sector is stored",
+			Value: "",
+		},
+		&cli.StringFlag{
+			Name:  "actor",
+			Usage: "miner actor id",
+		},
+	},
+	Action: func(cctx *cli.Context) error {
+		deadlineID := cctx.Int("deadline")
+		if deadlineID < 0 || deadlineID > 47 {
+			return xerrors.New("--deadline must be between 0 and 47")
+		}
+
+		nodeApi, closer, err := lcli.GetFullNodeAPIV1(cctx)
+		if err != nil {
+			return err
+		}
+		defer closer()
+
+		maddr, err := util.GetActorAddress(cctx)
+		if err != nil {
+			return err
+		}
+
+		amid, err := addr.IDFromAddress(maddr)
+		if err != nil {
+			return err
+		}
+
+		sdir, err := getSdir(cctx)
+		if err != nil {
+			return err
+		}
+
+		sb, err := ffiwrapper.New(&basicfs.Provider{
+			Root: sdir,
+		})
+		if err != nil {
+			return err
+		}
+
+		head, err := nodeApi.ChainHead(context.Background())
+		if err != nil {
+			return err
+		}
+
+		mact, err := nodeApi.StateGetActor(context.Background(), maddr, head.Key())
+		if err != nil {
+			return err
+		}
+
+		tbs := blockstore.NewTieredBstore(blockstore.NewAPIBlockstore(nodeApi), blockstore.NewMemory())
+		mas, err := miner.Load(adt.WrapStore(context.Background(), cbor.NewCborStore(tbs)), mact)
+		if err != nil {
+			return err
+		}
+
+		dl, err := mas.LoadDeadline(uint64(deadlineID))
+		if err != nil {
+			return err
+		}
+
+		err = dl.ForEachPartition(func(idx uint64, part miner.Partition) error {
+			liveSector, err := part.LiveSectors()
+			if err != nil {
+				return err
+			}
+
+			sInfo, err := getSectorInfo(nodeApi, maddr, liveSector)
+			if err != nil {
+				return err
+			}
+
+			err = emulator(sb, abi.ActorID(amid), sInfo)
+			if err != nil {
+				log.Warnw("wdpost emulator err", "deadlineID", deadlineID, "partitionID", idx)
+				return err
+			}
+
+			return nil
+		})
+
+		return nil
+	},
+}
+
+func getSdir(cctx *cli.Context) (string, error) {
+	sdir := cctx.String("sdir")
+	if sdir == "" {
+		return "", xerrors.New("--sdir err")
+	}
+	return sdir, nil
+}
+
+func getSectorInfo(nodeApi api.FullNode, maddr addr.Address, sectors bitfield.BitField) ([]proof.SectorInfo, error) {
+	ss, err := nodeApi.StateMinerSectors(context.Background(), maddr, &sectors, types.EmptyTSK)
+	if err != nil || len(ss) == 0 {
+		return nil, err
+	}
+
+	substitute := proof.SectorInfo{
+		SectorNumber: ss[0].SectorNumber,
+		SealedCID:    ss[0].SealedCID,
+		SealProof:    ss[0].SealProof,
+	}
+
+	sectorByID := make(map[uint64]proof.SectorInfo, len(ss))
+	for _, sector := range ss {
+		sectorByID[uint64(sector.SectorNumber)] = proof.SectorInfo{
+			SectorNumber: sector.SectorNumber,
+			SealedCID:    sector.SealedCID,
+			SealProof:    sector.SealProof,
+		}
+	}
+
+	proofSectors := make([]proof.SectorInfo, 0, len(ss))
+	if err := sectors.ForEach(func(sectorNo uint64) error {
+		if info, found := sectorByID[sectorNo]; found {
+			proofSectors = append(proofSectors, info)
+		} else {
+			proofSectors = append(proofSectors, substitute)
+		}
+		return nil
+	}); err != nil {
+		return nil, xerrors.Errorf("iterating partition sector bitmap: %w", err)
+	}
+
+	return proofSectors, nil
+}
+
+func emulator(sb *ffiwrapper.Sealer, aid abi.ActorID, sInfo []proof.SectorInfo) error {
+	var challenge [32]byte
+	rand.Read(challenge[:])
+	proofs, skp, err := sb.GenerateWindowPoSt(context.Background(), aid, sInfo, challenge[:])
+	if err != nil {
+		return err
+	}
+
+	if len(skp) != 0 {
+		log.Error("skip sectors: ", skp)
+	}
+
+	ok, err := ffiwrapper.ProofVerifier.VerifyWindowPoSt(context.TODO(), proof.WindowPoStVerifyInfo{
+		Randomness:        challenge[:],
+		Proofs:            proofs,
+		ChallengedSectors: sInfo,
+		Prover:            aid,
+	})
+	if err != nil || !ok {
+		log.Error("window post verification failed")
+		return err
+	}
+
+	log.Info("wdpost simulation is successful")
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -3,9 +3,17 @@ module lotus-box
 go 1.16
 
 require (
+	github.com/filecoin-project/filecoin-ffi v0.30.4-0.20200910194244-f640612a1a1f
+	github.com/filecoin-project/go-address v0.0.5
+	github.com/filecoin-project/go-bitfield v0.2.4
+	github.com/filecoin-project/go-state-types v0.1.1-0.20210915140513-d354ccf10379
 	github.com/filecoin-project/lotus v1.13.0
+	github.com/filecoin-project/specs-actors/v2 v2.3.5
+	github.com/filecoin-project/specs-storage v0.1.1-0.20201105051918-5188d9774506
+	github.com/ipfs/go-ipld-cbor v0.0.5
 	github.com/ipfs/go-log/v2 v2.3.0
 	github.com/urfave/cli/v2 v2.2.0
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
 )
 
 replace github.com/filecoin-project/filecoin-ffi => ../lotus/extern/filecoin-ffi
